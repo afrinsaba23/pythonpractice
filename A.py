8804e2bcca1a6a6ef9caadcb9ab360e0ba4abb5b
asnan <mdasnankrn@gmail.com>
2025-11-10 12:43:19 +0530

    added duplicates program for chatbox

---
+++ A.py
@@ -0,0 +1,10 @@
+#remove duplicate words form a string  
+def remove_duplicate_words(input_string):
+    words = input_string.split()
+    unique_words = list(dict.fromkeys(words))
+    return ' '.join(unique_words)
+
+input_string = "This is is a test test string string"
+result = remove_duplicate_words(input_string)
+print(result)
+# Output: "This is a test string"
